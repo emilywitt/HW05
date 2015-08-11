428b9b6103c9d92dbeb1f866588c9cb8ee4bc9ae
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-10 22:47:27 -0700

    EW HW 09_01

--- HW05_ex09_01.py
+++ HW05_ex09_01.py
@@ -7,11 +7,21 @@
 # Imports
 
 # Body
+def big_words():
+	"""read a given file line by line and prints words that are more than 20 characters in length"""
+	fin = open('words.txt')
+	line = fin.readline()
+	for line in fin:
+		word = line.strip()
+		if len(word) >= 20:
+			print (word)
 
 
 ##############################################################################
 def main():
-    pass # Call your functions here.
+    #pass # Call your functions here.
+
+    big_words()
 
 if __name__ == '__main__':
     main()
